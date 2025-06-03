d402be5aa953d287cdff5fc0b1bce558d4729489
K0ron <kevin.caron.91@gmail.com>
2025-06-03 15:47:53 +0200

    cours 56

---
+++ 05-Variable_nombres/39_monais/main.py
@@ -0,0 +1,12 @@
+achat_1 = 12.4
+quantite_1 = 2
+achat_2 = 7.5
+quantite_2 = 3
+achat_3 = 2.6
+quantite_3 = 2
+
+argent = 100
+
+monnaie = argent - (achat_1*quantite_1 + achat_2*quantite_2 + achat_3*quantite_3) 
+
+print("Monnaie restante :", monnaie)
